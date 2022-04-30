5b55751905908012ad393277a8d3ae1546bc6207
SKShehan <shehansilva2013@gmail.com>
2022-04-30 23:05:24 +0530

    ENV File added

--- server.js
+++ server.js
@@ -13,24 +13,24 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
-/* 
-MongoDB Connection
+
+// MongoDB Connection
 
 const URL = process.env.MONGODB_URL;
 
 mongoose.connect(URL, {
-  useCreateIndex: true,
-  useNewUrlParser: true,
+//  useCreateIndex: true,
+//  useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
+//  useFindAndModify: false,
 });
 
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("Mongo DB connection success!");
-}); 
+});
 
-*/
+
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port number ${PORT}`);
