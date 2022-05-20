f70864e04ef99aa612530c64603e38e4eb0d5883
IT19967080 <it19967080@my.sliit.lk>
2022-05-21 00:01:50 +0530

    Update staff.js

--- models/staff.js
+++ models/staff.js
@@ -3,14 +3,13 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const staffSchema = new Schema({
-    fullName : {type:String, required:true},
-    Sliitemail : {type:String, required:true},
+    firstName : {type:String, required:true},
+    lastName : {type:String, required:true},
+    sliitEmail : {type:String, required:true},
     staffId  :{type:String, required:true},
-    mobileNo : {type:String, required:true},
+    telNo : {type:String, required:true},
     field : {type:String, required:true},
     password : {type:String, required:true},
-    role : {type:String, required:true},
-    
 })
 
 const Staff = mongoose.model("staff",staffSchema);
